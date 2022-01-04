1c27d1bdc9e15bb024ffbb20740670bc34549c1c
Sarry-Mac <sarryqassir@hotmail.com>
2022-01-03 23:55:46 -0500

    commit yay

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/personal-website" element={<Home />} />
         <Route path="/products" element={<Products />} />
       </Routes>
     </Router>
