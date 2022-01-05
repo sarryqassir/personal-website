f021d51272f852db51e2e80c2b7f279d32d9f881
Sarry-Mac <sarryqassir@hotmail.com>
2022-01-04 23:14:34 -0500

    added index to new assets folder

---
+++ src/assets/index.js
@@ -0,0 +1,14 @@
+const assets = {
+  videos: {
+    zombsHighlights: require("./zombs-highlights.mp4"),
+    tarikOverpass: require("./tarik-overpass.mp4"),
+    twistzzSitDown: require("./twistzz-sit-the-f-down.mp4"),
+    tarikCereal: require("./tarik-cereal-4k.mp4"),
+  },
+
+  images: {
+    tarik: require("./img-1.jpg"),
+    alhamdulillah: require("./img-2.png"),
+  },
+};
+export default assets;
