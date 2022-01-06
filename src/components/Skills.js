a6597932f4035f3cc1eb7701b2ac843597057853
Sarry-Mac <sarryqassir@hotmail.com>
2022-01-05 23:41:18 -0500

    added react-tooltip and cleaned up redundant code

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,13 +4,13 @@ import "./Skills.css";
 import skills from "./skills.json";
 import { v4 as uuidv4 } from "uuid";
 import { TransitionGroup } from "react-transition-group";
+import ReactTooltip from "react-tooltip";
+import { capitalizeFirst } from "./Utils.js";
 
 function Skills() {
   const [search, setSearch] = useState();
   const [sortOrder, setSortOrder] = useState("descending");
   const [sorting, setSorting] = useState("rating");
-
-  console.log(search);
   // filterSkills = (e) => {
   //   let keyword = e.target.value;
   //   setSearch(keyword);
@@ -26,66 +26,75 @@ function Skills() {
       : -1
   );
 
-  function handleSortStars() {
-    if (sorting !== "rating") {
-      setSorting("rating");
+  function handleSort(item) {
+    if (sorting !== item) {
+      setSorting(item);
       setSortOrder("descending");
     } else
       setSortOrder(sortOrder === "descending" ? "ascending" : "descending");
   }
 
-  function handleSortNames() {
-    if (sorting !== "name") {
-      setSorting("name");
-      setSortOrder("descending");
-    } else
-      setSortOrder(sortOrder === "descending" ? "ascending" : "descending");
-  }
+  const dynamicSortIcon = (type) =>
+    sorting === type
+      ? sortOrder === "descending"
+        ? "fa-solid fa-sort-down"
+        : sortOrder === "ascending"
+        ? "fa-solid fa-sort-up"
+        : "fa-solid fa-sort"
+      : "fa-solid fa-sort";
+
+  const dynamicTooltip = (type) =>
+    sorting === type
+      ? sortOrder === "descending" || sortOrder === "ascending"
+        ? capitalizeFirst(sortOrder)
+        : "Sort by " + capitalizeFirst(type)
+      : "Sort by " + capitalizeFirst(type);
 
   return (
     <div className="skills-container">
       <h1>Skills</h1>
       <hr />
-      {/* filter bar, maybe make items selectable/ favourites
+      {/* maybe make items selectable/ favourites
       filter how many stars
       drop down menu to sort by type (program/language, idea/theory, etc)
       */}
-      <div className="filters">
-        <button
-          id="name-sort-btn"
-          className={
-            sorting === "name"
-              ? sortOrder === "descending"
-                ? "fa-solid fa-sort-down"
-                : sortOrder === "ascending"
-                ? "fa-solid fa-sort-up"
-                : "fa-solid fa-sort"
-              : "fa-solid fa-sort"
-          }
-          type="button"
-          onClick={handleSortNames}
-        />
+      <div className="skill-filters">
+        {/* make mini button compontent */}
+        <div className="name-sort-btn-div">
+          <button
+            key="name-sort-btn"
+            id="name-sort-btn"
+            className={dynamicSortIcon("name")}
+            type="button"
+            data-tip={dynamicTooltip("name")}
+            data-for="name-btn"
+            onClick={() => handleSort("name")}
+          />
+          <ReactTooltip key={"name-btn-tooltip"} id="name-btn" effect="solid" />
+        </div>
         <input
           className="filter-bar"
           type="text"
-          placeholder="Filter through skills"
+          placeholder="Filter skills"
           onChange={(e) => setSearch(e.target.value)}
         />
         {/* if its clicked a lot, ask user if third option (no sort) is good or should be removed */}
-        <button
-          id="star-sort-btn"
-          className={
-            sorting === "rating"
-              ? sortOrder === "descending"
-                ? "fa-solid fa-sort-down"
-                : sortOrder === "ascending"
-                ? "fa-solid fa-sort-up"
-                : "fa-solid fa-sort"
-              : "fa-solid fa-sort"
-          }
-          type="button"
-          onClick={handleSortStars}
-        />
+        <div className="rating-sort-btn-div">
+          <button
+            key="rating-sort-btn"
+            id="rating-sort-btn"
+            className={dynamicSortIcon("rating")}
+            type="button"
+            data-tip={dynamicTooltip("rating")}
+            data-for="rating-btn"
+            onClick={() => handleSort("rating")}
+          />
+          <ReactTooltip
+            key="rating-btn-tooltip"
+            id="rating-btn"
+            effect="solid"
+          />
+        </div>
       </div>
       <TransitionGroup>
         <ul className="skills-list">
@@ -95,6 +104,7 @@ function Skills() {
               if (!search) return skill;
               else if (skill.name.toLowerCase().includes(search.toLowerCase()))
                 return skill;
+              return false;
             })
             .map((skill) => {
               // move csstransition here
@@ -111,7 +121,7 @@ function Skills() {
             })}
         </ul>
       </TransitionGroup>
-      <p>this project was made using React</p>
+      <p>This project was made using React</p>
       <a
         href="https://github.com/sarryqassir/personal-website"
         target="_blank"
