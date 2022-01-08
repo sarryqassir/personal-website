21ff9f0520846dfe804bf614f13487e3b5536200
Sarry-Mac <sarryqassir@hotmail.com>
2022-01-08 06:39:54 -0500

    changed to HashRouter so routing works with pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/pages/Home";
-import Products from "./components/pages/Products";
+// import Products from "./components/pages/Products";
+import TodoListApp from "./components/TodoListApp";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/todolist" element={<TodoListApp />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
-
-// redirect 404, /*
-// wtf is 404
-// public url thing to start from home page
 
 export default App;
