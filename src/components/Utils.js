9b17e714ef8467bb48f90e1c3a87100d504b27a1
Sarry-Mac <sarryqassir@hotmail.com>
2022-01-08 15:35:11 -0500

    calculate age based on date string

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -28,3 +28,13 @@ export function useWindowSize() {
   }, []);
   return size;
 }
+
+// Calculate age based on date string
+export function calculateAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) age--;
+  return age;
+}
