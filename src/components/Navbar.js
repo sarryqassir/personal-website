c87926ae49a5cc88b7d1fae4e9cf00a2aa2554cd
Sarry-Mac <sarryqassir@hotmail.com>
2022-01-08 06:44:28 -0500

    removed unused buttons & changed logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,23 +25,22 @@ function Navbar() {
   window.addEventListener("resize", showButton);
 
   return (
-    <>
-      <nav className="navbar">
-        {/* <div className="navbar-container"> */}
-        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-          LOGO
-          <i className="fab fa-typo3" />
-        </Link>
-        <div className="menu-icon" onClick={handleClick}>
-          <i className={click ? "fas fa-times" : "fas fa-bars"} />
-        </div>
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
-          <li className="nav-item">
-            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
+    <nav className="navbar">
+      {/* <div className="navbar-container"> */}
+      <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+        <span className="nerd-emoji">🤓</span>
+        Sarry
+      </Link>
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={click ? "fas fa-times" : "fas fa-bars"} />
+      </div>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li className="nav-item">
+          <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+            Home
+          </Link>
+        </li>
+        {/* <li className="nav-item">
             <Link
               to="/services"
               className="nav-links"
@@ -49,30 +48,25 @@ function Navbar() {
             >
               Projects
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/products"
-              className="nav-links"
-              onClick={closeMobileMenu}
-            >
-              Products
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/sign-up"
-              className="nav-links-mobile"
-              onClick={closeMobileMenu}
-            >
-              Sign Up
-            </Link>
-          </li>
-        </ul>
-        {button && <Button buttonStyle="btn--outline">PROJECTS</Button>}
-        {/* </div> */}
-      </nav>
-    </>
+          </li> */}
+        <li className="nav-item">
+          <Link to="/todolist" className="nav-links" onClick={closeMobileMenu}>
+            To Do List
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/sign-up"
+            className="nav-links-mobile"
+            onClick={closeMobileMenu}
+          >
+            Sign Up
+          </Link>
+        </li>
+      </ul>
+      {button && <Button buttonStyle="btn--outline">PROJECTS</Button>}
+      {/* </div> */}
+    </nav>
   );
 }
 
