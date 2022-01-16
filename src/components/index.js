b2194641ca85535e37d6b882da6e2c47113bb132
Sarry-Mac <sarryqassir@hotmail.com>
2022-01-16 18:15:23 -0500

    index for modules

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Navbar } from "./Navbar";
+export { default as Home } from "./pages/Home";
+export { default as Signup } from "./Signup";
+export { default as Signin } from "./Signin";
+export { default as TodoListApp } from "./TodoListApp";
+export { default as Account } from "./Account";
+export { default as ForgotPassword } from "./ForgotPassword";
