e734876e1e302089ecd22a29e4a470b103432a3c
Sarry-Mac <sarryqassir@hotmail.com>
2022-01-16 18:12:48 -0500

    added auth provider and routes for user auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Home from "./components/pages/Home";
-// import Products from "./components/pages/Products";
-import TodoListApp from "./components/TodoListApp";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import { AuthProvider, useAuth } from "./contexts/AuthContext";
+import {
+  Navbar,
+  Home,
+  TodoListApp,
+  Signup,
+  Signin,
+  Account,
+  ForgotPassword,
+} from "./components/index";
+
+/** Protects child route from users that aren't signed in, redirecting them to the sign in page.  */
+export const PrivateRoute = (redir: { redir: string }) => {
+  const { currentUser } = useAuth();
+  return currentUser ? <Outlet /> : <Navigate to={"/" + redir} />;
+};
 
 function App() {
   return (
-    <HashRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/todolist" element={<TodoListApp />} />
-      </Routes>
-    </HashRouter>
+    <Router>
+      <AuthProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/todolist" element={<TodoListApp />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          {/* Protected Route  */}
+          <Route path="/account" element={<PrivateRoute redir="signin" />}>
+            <Route path="/account" element={<Account />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </Router>
   );
 }
 
